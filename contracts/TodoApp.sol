45fae031615280b2e5cfb8323be1671c6e5225dd
Emre <emre@Emres-MBP.fritz.box>
2022-04-08 19:26:38 +0200

    openzeplin ownable interface added and add/remevo function turn into closable openable function via flag

--- contracts/TodoApp.sol
+++ contracts/TodoApp.sol
@@ -3,10 +3,20 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract TodoApp {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TodoApp is Ownable {
     string[] public items;
+    bool public isDeletable;
+    bool public isAddable;
+
+    constructor() {
+        isDeletable = false;
+        isAddable = true;
+    }
 
     function addItem(string memory _item) public {
+        require(isAddable == true, "Add functionality deactivated by admin");
         items.push(_item);
     }
 
@@ -19,10 +29,28 @@ contract TodoApp {
     }
 
     function removeItem(uint256 _index) public {
+        require(
+            isDeletable == true,
+            "Delete functionality deactivated by admin"
+        );
         require(_index < items.length, "index out of bound");
         for (uint256 i = _index; i < items.length - 1; i++) {
             items[i] = items[i + 1];
         }
         items.pop();
+    }
+
+    function activateDeleteFeature() public onlyOwner {
+        require(isDeletable == false, "Delete functionality already active");
+        isDeletable = true;
+    }
+
+    function activateAddFeature() public onlyOwner {
+        require(isAddable == true, "Add functionality already active");
+        isAddable = true;
+    }
+
+    function deleteAllItem() public onlyOwner {
+        items = new string[](0);
     }
 }
